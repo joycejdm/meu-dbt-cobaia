758122028b35fd3462c0ff5da9e94b0d2af351b0
Joyce <joycedionizio.jdm@gmail.com>
2025-10-31 13:36:49 -0300

    feat: Testa mudança de dados (valor * 2)

--- dbt_cobaia_project/models/marts/fct_vendas.sql
+++ dbt_cobaia_project/models/marts/fct_vendas.sql
@@ -1,7 +1,6 @@
--- models/marts/fct_vendas.sql
 select
     id_venda,
     data_venda,
-    valor
+    valor * 2 as valor -- AQUI ESTÁ A MUDANÇA!
 from
-    {{ ref('vendas') }} -- Aponta para o CSV que acabou de carregar com o 'dbt seed'
+    {{ ref('vendas') }}
